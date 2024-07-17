b25dfecf0905fb425d37d4061a67c1a9eb45eecc
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-07-17 11:29:32 -0600

    Update user entity

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,20 +1,27 @@
 export class User {
-  id: number;
+  id?: number;
   firstName: string;
   lastName: string;
   email: string;
   username: string;
   password: string;
 
-  constructor(
-    id: number,
-    firstName: string,
-    lastName: string,
-    email: string,
-    username: string,
-    password: string,
-  ) {
-    this.id = id;
+  constructor({
+    id,
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+  }: {
+    id?: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    password: string;
+  }) {
+    if (id) this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
