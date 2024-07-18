1faf2cad1a2e84486791bb89ce6cebdb8f6da565
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-07-17 22:45:40 -0600

    Add functionality to update password

--- src/app/modules/users/pages/update-password/update-password.component.ts
+++ src/app/modules/users/pages/update-password/update-password.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { firstValueFrom, Subject, takeUntil } from 'rxjs';
+
+import { PasswordValidatorService } from '@core/services/validators';
+import { FormGroupDirective } from '@shared/directives';
+import { UpdatePasswordForm, UpdateUserPassword } from '../../types';
+import { UserService } from '../../services';
+import { ErrorResponse } from '@core/types';
+import { AlertComponent } from '@shared/components';
 
 @Component({
   selector: 'app-update-password',
   standalone: true,
-  imports: [],
+  imports: [FormGroupDirective, ReactiveFormsModule, AlertComponent],
   templateUrl: './update-password.component.html',
   styles: ``,
 })
-export default class UpdatePasswordComponent {}
+export default class UpdatePasswordComponent implements OnInit, OnDestroy {
+  passwordForm!: FormGroup<UpdatePasswordForm>;
+  isLoading = false;
+  formError: string | null = null;
+  #userId = 0;
+  #fb = inject(FormBuilder);
+  #route = inject(ActivatedRoute);
+  #passwordValidatorService = inject(PasswordValidatorService);
+  #userService = inject(UserService);
+  #router = inject(Router);
+  #unsubscribeAll$ = new Subject<void>();
+
+  get disableSubmit(): boolean {
+    return this.passwordForm.invalid || this.isLoading || !!this.formError;
+  }
+
+  ngOnInit(): void {
+    this.#createForm();
+    this.#route.params
+      .pipe(takeUntil(this.#unsubscribeAll$))
+      .subscribe(({ id }) => (this.#userId = id));
+  }
+
+  ngOnDestroy(): void {
+    this.#unsubscribeAll$.next();
+    this.#unsubscribeAll$.complete();
+  }
+
+  async onSubmit(): Promise<void> {
+    if (this.disableSubmit) return;
+    this.isLoading = true;
+    const { passwordConfirmation, ...pass } = this.passwordForm.value;
+
+    try {
+      await firstValueFrom(
+        this.#userService.updateUserPassword(
+          this.#userId,
+          pass as UpdateUserPassword,
+        ),
+      );
+      this.#router.navigate(['/users']);
+    } catch (error: any) {
+      const errorResponse = error.error as ErrorResponse;
+      this.formError = errorResponse.message;
+      setTimeout(() => (this.formError = null), 5000);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  #createForm(): void {
+    this.passwordForm = this.#fb.group(
+      {
+        currentPassword: [
+          '',
+          [
+            Validators.required,
+            this.#passwordValidatorService.passwordPatternValidator(
+              new RegExp(PasswordValidatorService.passwordPattern),
+            ),
+          ],
+        ],
+        newPassword: [
+          '',
+          [
+            Validators.required,
+            this.#passwordValidatorService.passwordPatternValidator(
+              new RegExp(PasswordValidatorService.passwordPattern),
+            ),
+          ],
+        ],
+        newPasswordConfirmation: ['', [Validators.required]],
+      },
+      {
+        validators: [
+          this.#passwordValidatorService.passwordsMatchValidator(
+            'newPassword',
+            'newPasswordConfirmation',
+          ),
+        ],
+      },
+    );
+  }
+}
